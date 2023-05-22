c320b1cca0af02cb786963fb523d7c287c61afba
Francesco-Iannuzzi <iannuzzi.francesco@outlook.com>
2023-05-22 16:20:54 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- /bootstrap style -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- /fontawesome -->
+    <title>Welcome Laravel</title>
+</head>
+
+<body class="bg-dark">
+
+    <header>
+        <nav class="nav justify-content-center py-3 bg-dark">
+            <a class="nav-link text-danger" href="{{ route('home') }}">
+                <h6>Home</h6>
+            </a>
+            <a class="nav-link" href="{{ route('about') }}">
+                <h6>About</h6>
+            </a>
+            <a class="nav-link" href="{{ route('contact') }}">
+                <h6>Contact</h6>
+            </a>
+        </nav>
+        <!-- /navbar -->
+
+    </header>
+    <!-- /header -->
+
+    <main>
+        <section class="jumbo">
+            <div class="p-5 bg-light rounded-3">
+                <div class="container-fluid py-5">
+                    <div class="d-flex">
+                        <h1 class="display-5 fw-bold text-danger me-4 mb-0">Welcome on Laravel</h1>
+                        <i class="fa-brands fa-laravel fa-2x align-self-center text-danger"></i>
+                    </div>
+                    <p class="col-md-8 fs-4">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <!-- /jumbo -->
+
+        <section class="home bg-dark py-5">
+            <div class="text-center text-light">
+                <h1>{{$greeting}}</h1>
+            </div>
+        </section>
+        <!-- /home -->
+    </main>
+    <!-- /main -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+    <!-- /bootstrap script -->
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    $data = [
+        'greeting' => 'Hello World'
+    ];
+    return view('home', $data);
+})->name('home');
+
+
+Route::get('/about', function () {
+    $page_title = 'About Us';
+    $description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    return view('about', compact('description', 'page_title'));
+})->name('about');
+
+
+Route::get('/contact', function () {
+    $data_contact = [
+        'page_title' => 'Contact',
+        'contacts' => [
+            'Mario Rossi: 1234567890',
+            'Luigi Bianchi: 0987654321',
+            'Giovanna Verdi: 6789012345',
+            'Francesco Celeste: 5432109876',
+            'Luca Neri: 2143658709',
+            'Maria Gialli: 3216549870',
+            'Lucia Grigio: 78956723410'
+        ]
+    ];
+    return view('contact', $data_contact);
+})->name('contact');
